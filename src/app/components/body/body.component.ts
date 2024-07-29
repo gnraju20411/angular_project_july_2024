25bdb9ffd11aaeb9a593ea87e3823619f3925cd6
nagaraju grandhi <nagaraju@hawkeslearning.com>
2024-07-29 18:08:09 +0530

    Added Card Component

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
