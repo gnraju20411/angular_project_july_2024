8a52f284a7111a95bb3dfcfc456bf9e44b91f8fb
nagaraju <nagaraju@hawkeslearning.com>
2024-08-05 00:57:21 +0530

    Card-Table component

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { DatabindingComponent } from "../databinding/databinding.component";
+import { CardTableComponent } from "../card-table/card-table.component";
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [CardComponent, DatabindingComponent],
+  imports: [CardComponent, DatabindingComponent, CardTableComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
---
+++ src/app/components/card-table/card-table.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+
+@Component({
+  selector: 'app-card-table',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './card-table.component.html',
+  styleUrl: './card-table.component.css'
+})
+export class CardTableComponent {
+  flag:Boolean=false;
+  products = [
+    {Id: 1, Name: "Iphone 15", Color: "Pink", Prise: 50000, Description: "Testing the cards...", ImageUrl: "...", Status: true},
+    {Id: 2, Name: "Iphone 14", Color: "Black", Prise: 45000, Description: "Testing the cards...", ImageUrl: "...", Status: true},
+    {Id: 3, Name: "Iphone 13", Color: "Pink", Prise: 55000, Description: "Testing the cards...", ImageUrl: "...", Status: true},
+    {Id: 4, Name: "Iphone 12", Color: "Red", Prise: 12548, Description: "Testing the cards...", ImageUrl: "...", Status: true},
+    {Id: 5, Name: "OnePlus 9R", Color: "Pink", Prise: 45847, Description: "Testing the cards...", ImageUrl: "...", Status: true},
+    {Id: 6, Name: "Iphone X", Color: "Blue", Prise: 55884, Description: "Testing the cards...", ImageUrl: "...", Status: true},
+    {Id: 7, Name: "OnePlus 12R", Color: "Blue", Prise: 45874, Description: "Testing the cards...", ImageUrl: "...", Status: true},
+    {Id: 8, Name: "Iphone SE", Color: "Black", Prise: 25000, Description: "Testing the cards...", ImageUrl: "...", Status: true},
+    {Id: 9, Name: "Iphone 7", Color: "Green", Prise: 18000, Description: "Testing the cards...", ImageUrl: "...", Status: true}
+  ]
+}
